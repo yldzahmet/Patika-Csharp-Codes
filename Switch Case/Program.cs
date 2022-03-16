fea897625f0b1ef0379bccf69ce677dc028d688f
Ahmet <ahmet.yldz.207@gmail.com>
2022-03-16 14:13:08 +0300

    switch case statements

---
+++ Switch Case/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Switch_Case
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int month = DateTime.Now.Month;
+            switch(month)
+            {
+                case 1:
+                    Console.WriteLine("Ocak Ayındasınız.");
+                    break;
+                case 2:
+                    Console.WriteLine("Şubat Ayındasınız.");;
+                    break;
+                case 3:
+                    Console.WriteLine("Mart Ayındasınız.");;
+                    break;
+                case 4:
+                    Console.WriteLine("Nisan Ayındasınız.");;
+                    break;
+                default:
+                    Console.WriteLine("Yanlış Veri Dönüşü.");;
+                    break;
+            }
+
+            switch(month)
+            {
+                case 12:
+                case 1:
+                case 2:
+                    Console.WriteLine("Kış Mevsimindesiniz.");
+                    break;
+                case 3:
+                case 4:
+                case 5:
+                    Console.WriteLine("İlkbahar Mevsimindesiniz.");
+                    break;
+                case 6:
+                case 7:
+                case 8:
+                    Console.WriteLine("Yaz Mevsimindesiniz.");
+                    break;
+                case 9:
+                case 10:
+                case 11:
+                    Console.WriteLine("Sonbahar Mevsimindesiniz.");
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
