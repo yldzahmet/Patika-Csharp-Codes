cfdeb984b07d29fc279260735e7174da22d1eda1
Ahmet <ahmet.yldz.207@gmail.com>
2022-03-16 13:43:53 +0300

    if else, ternary if

---
+++ if else ternary if/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace if_else_ternary_if
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int time = DateTime.Now.Hour;
+            if (time >= 6 && time < 11)
+            {
+                Console.WriteLine("Günaydın.");
+            }
+            else if(time <= 18)
+            {
+                Console.WriteLine("İyi günler.");
+
+            }
+            else
+            {
+                Console.WriteLine("İyi geceler.");
+            }
+
+            string result = time <= 18 ? "İyi günler." : "İyi geceler.";
+            Console.WriteLine(result);
+
+            string result2 = time >= 6 ? "İyi günler." : time <= 18 ? "İyi günler." : "İyi geceler.";
+        }
+    }
+}
