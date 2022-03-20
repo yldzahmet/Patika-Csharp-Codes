adbc03500577421ea5af32c072a1b8b867133967
Ahmet <ahmet.yldz.207@gmail.com>
2022-03-20 23:19:01 +0300

    Metot Tanımlama

---
+++ Method Definition/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Method_Definition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 2;
+            int b = 3;
+            Console.WriteLine(a - b);
+
+            int sonuc = Topla(a, b);
+            Console.WriteLine(sonuc);
+
+            Methodlar ornek = new Methodlar();
+            ornek.EkranaYazdır(Convert.ToString(sonuc));
+
+            Console.WriteLine(ornek.ArttırVeTopla(ref a, ref b)); 
+            ornek.EkranaYazdır(Convert.ToString(a + b));
+        }
+
+        static int Topla(int x, int y)
+        {
+            return x + y;
+        }
+    }
+
+    class Methodlar
+    {
+        public void EkranaYazdır(string veri)
+        {
+            Console.WriteLine(veri);
+        }
+
+        public int ArttırVeTopla(ref int a, ref int b)
+        {
+            a++;
+            b++;
+            return a + b;
+        }
+    }
+}
