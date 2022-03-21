f01ed5ab423d1c037d190aa790940b2dee760e0e
Ahmet <ahmet.yldz.207@gmail.com>
2022-03-21 12:46:01 +0300

    Algoritma Soruları

---
+++ Ödev 1/Ödev 1-1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Ödev_1_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number");
+            int arCount = Convert.ToInt32(Console.ReadLine().Trim());
+            Console.WriteLine("Enter {0} numbers: ", arCount);
+
+            List<int> list = Console.ReadLine()
+            .TrimEnd()
+            .Split(" ")
+            .ToList()
+            .Select(a => Convert.ToInt32(a))
+            .ToList();
+
+            Console.WriteLine("Even numbers in list: ");
+            foreach (var item in list)
+            {
+                if(item % 2 == 0)
+                    Console.WriteLine(item);
+            }
+        }
+    }
+}
