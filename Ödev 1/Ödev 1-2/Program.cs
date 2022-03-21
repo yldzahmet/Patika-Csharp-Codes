42aaf60218dcf1e5bc927e6cf4d3dafd124ed058
Ahmet <ahmet.yldz.207@gmail.com>
2022-03-21 12:48:09 +0300

    Ödev 1-2

---
+++ Ödev 1/Ödev 1-2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace Ödev_1_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter 2 numbers:");
+
+            List<int> divisor = Console.ReadLine()
+            .TrimEnd()
+            .Split(" ")
+            .ToList()
+            .Select(a => Convert.ToInt32(a))
+            .ToList();
+
+            Console.WriteLine("Enter {0} numbers:", divisor[1]);
+
+            List<int> numbers = Console.ReadLine()
+            .TrimEnd()
+            .Split(" ")
+            .ToList()
+            .Select(a => Convert.ToInt32(a))
+            .ToList();
+
+            int m = divisor[0];
+            Console.WriteLine("Divisors of {0} ", m);
+            foreach (var item in numbers)
+            {
+                if(m % item == 0)
+                {
+                    Console.WriteLine(item);
+                }
+                    
+            }
+
+
+
+        }
+    }
+}
