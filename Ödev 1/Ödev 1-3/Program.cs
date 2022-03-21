56c8ab697925458d90a5794737c0e15466aee582
Ahmet <ahmet.yldz.207@gmail.com>
2022-03-21 12:49:16 +0300

    Ödev 1-3

---
+++ Ödev 1/Ödev 1-3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Ödev_1_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number:");
+            int number = Convert.ToInt32(Console.ReadLine().TrimEnd()); 
+
+            Console.WriteLine("Enter a sentence with {0} word:", number);
+
+            List<string> words = Console.ReadLine()
+            .TrimEnd()
+            .Split(" ")
+            .ToList();
+            words.Reverse();
+
+            Console.WriteLine("Sentence in reverse order:");
+            foreach (var item in words)
+            {
+                Console.Write("{0} ", item);
+            }
+        }
+    }
+}
