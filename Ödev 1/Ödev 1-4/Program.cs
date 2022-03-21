06bd64e275ca66577689e21b520b8b8b7b0e4702
Ahmet <ahmet.yldz.207@gmail.com>
2022-03-21 12:49:57 +0300

    Ödev 1-4

---
+++ Ödev 1/Ödev 1-4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Ödev_1_4
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            int letterNumber = 0, wordNumber = 0;
+
+            Console.WriteLine("Enter a sentence.");
+
+            List<string> words = Console.ReadLine()
+            .TrimEnd()
+            .Split(" ")
+            .ToList();
+
+            wordNumber = words.Count;
+            foreach (var item in words)
+            {
+                letterNumber += item.Count(char.IsLetter);
+            }
+
+            Console.WriteLine("There are {0} words and {1} letters in sentence."
+            , wordNumber
+            , letterNumber);
+        }
+    }
+}
