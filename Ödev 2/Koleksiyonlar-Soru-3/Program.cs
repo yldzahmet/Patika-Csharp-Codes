31e3e2895b20c0f777a6247703925f2b8b3e16ca
Ahmet <ahmet.yldz.207@gmail.com>
2022-03-24 00:10:06 +0300

    Koleksiyonlar-Soru-3

---
+++ Ödev 2/Koleksiyonlar-Soru-3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+
+namespace Koleksiyonlar_Soru_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("İçerisindeki ünlü harfleri ayırmak için bir metin girin: ");
+            string str = Console.ReadLine();
+            string ünlüHarfler = "aeıioöuü";
+            ArrayList sequence = new ArrayList();
+            Console.WriteLine("Girilen metin içindeki ünlü harfler: ");
+            for (int i = 0; i < str.Length ; i++)
+            {
+                if( Char.IsLetter(str[i]) && 
+                    ünlüHarfler.Contains( Char.ToLower(str[i]) ) 
+                    )
+                    {
+                    sequence.Add(str[i]);
+                    Console.Write( Char.ToLower(str[i]) );
+                    }
+            }
+        }
+    }
+}
